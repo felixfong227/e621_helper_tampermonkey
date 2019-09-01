12d69d870efe8351f11728f749b8076c388dda2d
Felix Fong <moongod101@hotmail.com>
2019-09-01 13:56:28 +0800

    The base constructor for all the E621 notification

--- Utils.js
+++ Utils.js
@@ -2,3 +2,9 @@
         if(msg) console.error('[e621.net Helper Utils Error] ' + msg);
     },
     _notifyConstru(opertor, msg, prefix) {
+        if(!opertor) throw new Error('Missing argument operator OwO');
+        var prefixMsg = '[e621.net Helper] ';
+        if(prefix === null || prefix === false) prefixMsg = '';
+        if(window[opertor] && typeof window.notice === 'function') return window[opertor].call(null, prefixMsg + msg);
+        return Utils.error('window.notice is not a valid function any more QvQ, WHYYYYYY DEV FROM E621!!!!!!!!!!!!!');
+    },
