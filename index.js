f98d127f24dadf053c51daab99cc395df917a6ff
Felix Fong <moongod101@hotmail.com>
2019-09-01 13:58:15 +0800

    Tampermonkey header tags

---
+++ index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         e621.net Helper
+// @namespace    RainbowTheDashie
+// @version      1.0.0
+// @description  Little util script to help make e621.net a much better place then it already is :P
+// @author       RainbowTheDashie
+// @match        https://e621.net/post/*
+// @match        http://e621.net/post/*
+// @match        https://e621.net/post
+// @match        http://e621.net/post
+// @run-at       document-end
+// @require      Utils.js
+// @require      Effects.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/bodymovin/4.4.27/bodymovin.min.js
+// @require      https://unpkg.com/tocca@2.0.4/Tocca.min.js
+// ==/UserScript==
